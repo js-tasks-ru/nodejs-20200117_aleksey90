b67229e80868912988be98d0ed6547f57a2562bd
Alexey Khabarov <aleksey.s.khabarov@gmail.com>
2020-02-01 16:33:22 +0300

    solved 4-module/2-task

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,6 +1,8 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
@@ -11,7 +13,38 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
-
+      if (pathname.indexOf('/') !== -1) {
+        res.statusCode = 400;
+        res.end(`Bad request`);
+      } else {
+        try {
+          fs.accessSync(filepath)
+          res.statusCode = 409;
+          res.end(`File already exists`);
+        } catch(err) {
+          req.on(`close`, () => {
+              if (req.aborted) {
+                fs.unlinkSync(filepath);
+                res.statusCode = 500;
+                res.end(`Connection aborted`)
+              };
+            })
+            .pipe(new LimitSizeStream({limit: 1048576}))
+            .on(`error`, err => {
+              fs.unlinkSync(filepath);
+              res.statusCode = 413;
+              res.end(`Big file`);
+            })
+            .pipe(fs.createWriteStream(filepath))
+            .on('error', err => {
+              res.statusCode = 500;
+            })
+            .on(`close`, () => {
+            res.statusCode = 201;
+            res.end(`closed`);
+          });
+        }
+      }
       break;
 
     default:
