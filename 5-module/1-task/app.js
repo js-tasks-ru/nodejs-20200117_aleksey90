d8ca6eda71ce54a281c6cc2fa3935801b1d7b149
Alexey Khabarov <aleksey.s.khabarov@gmail.com>
2020-02-07 09:07:17 +0300

    5-module/1-task start

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -7,12 +7,28 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+function requestDelay() {
+    return new Promise.all((resolve) => {
+        clients.push(resolve);
+    })
+}
+const clients = [];
 router.get('/subscribe', async (ctx, next) => {
+    ctx.body = await requestDelay();
+    return next();
 });
 
 router.post('/publish', async (ctx, next) => {
+    if (ctx.request.body.message) {
+        clients.forEach(item => {
+            item(ctx.request.body.message);
+        })
+    }
+    return next();
 });
 
 app.use(router.routes());
 
+
 module.exports = app;
+
