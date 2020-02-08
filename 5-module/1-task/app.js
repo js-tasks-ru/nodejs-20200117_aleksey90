d933037aef7ab6a1ce2e9573981fed0d8e108fda
Alexey Khabarov <aleksey.s.khabarov@gmail.com>
2020-02-09 00:11:20 +0300

    solved 5-module/1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -8,23 +8,24 @@ const Router = require('koa-router');
 const router = new Router();
 
 function requestDelay() {
-    return new Promise.all((resolve) => {
+    return new Promise((resolve) => {
         clients.push(resolve);
     })
 }
-const clients = [];
+let clients = [];
+
 router.get('/subscribe', async (ctx, next) => {
     ctx.body = await requestDelay();
-    return next();
 });
 
 router.post('/publish', async (ctx, next) => {
     if (ctx.request.body.message) {
         clients.forEach(item => {
             item(ctx.request.body.message);
-        })
+        });
+        ctx.status = 200;
+        clients = [];
     }
-    return next();
 });
 
 app.use(router.routes());
