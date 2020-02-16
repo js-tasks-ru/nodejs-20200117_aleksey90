8d2f25e7762f77d1e6681b85e26c87783d264301
Alexey Khabarov <aleksey.s.khabarov@gmail.com>
2020-02-16 09:37:04 +0300

    solved 6-module/2-task

--- 6-module/2-task/controllers/products.js
+++ 6-module/2-task/controllers/products.js
@@ -1,12 +1,22 @@
+const Product = require('../models/Product');
+const mapProduct = require('../mappers/product');
+const mongoose = require('mongoose');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+  ctx.productList = {};
+  if (ctx.request.query.subcategory) {
+    ctx.productList = {subcategory: ctx.request.query.subcategory};
+  }
+  return next();
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = await Product.find(ctx.productList);
+  ctx.body = (products) ? {products: products.map(mapProduct)} : {products: []};
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  if(!mongoose.Types.ObjectId.isValid(ctx.params.id)) ctx.throw(400);
+  const product = await Product.findById(ctx.params.id);
+  ctx.body = (product) ? {product: mapProduct(product)} : ctx.throw(404);
 };
-
