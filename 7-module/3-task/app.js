a4f81fd2388bffc27412cb3bea7d1a2f43c80fa6
Alexey Khabarov <aleksey.s.khabarov@gmail.com>
2020-03-01 00:37:52 +0300

    solved

--- 7-module/3-task/app.js
+++ 7-module/3-task/app.js
@@ -33,6 +33,8 @@ app.use((ctx, next) => {
   ctx.login = async function(user) {
     const token = uuid();
 
+    await Session.create({token, lastVisit: new Date(), user});
+    
     return token;
   };
 
@@ -44,6 +46,17 @@ const router = new Router({prefix: '/api'});
 router.use(async (ctx, next) => {
   const header = ctx.request.get('Authorization');
   if (!header) return next();
+  
+  const token = header.split(' ')[1];
+  if (!token) return next();
+
+  const session = await Session.findOne({token}).populate('user');
+  if (!session) return ctx.throw(401, `Неверный аутентификационный токен`);
+
+  await Session.findOneAndUpdate({token}, {lastVisit: new Date()});
+
+  ctx.user = session.user;
+
 
   return next();
 });
@@ -53,7 +66,7 @@ router.post('/login', login);
 router.get('/oauth/:provider', oauth);
 router.post('/oauth_callback', handleMongooseValidationError, oauthCallback);
 
-router.get('/me', me);
+router.get('/me', mustBeAuthenticated, me);
 
 app.use(router.routes());
 
